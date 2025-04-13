6be431f34caf54a620778041fc26780c87cdc576
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-04-13 20:27:26 +0300

    Add UserTaskStatus model

--- school_system/tasks/models.py
+++ school_system/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import User
 
 
 class Task(models.Model):
@@ -8,3 +9,16 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserTaskStatus(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('user', 'task')
+
+    def __str__(self):
+        return f"{self.user.email} - {self.task.name} - {'✔️' if self.is_completed else '❌'}"
