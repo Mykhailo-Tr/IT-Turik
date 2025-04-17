18074c4cb134feb2c4fd1ba358a94e9f8b348105
mox1tol <denisbaluk187@gmail.com>
2025-04-17 20:42:43 +0300

    test

--- school_system/tasks/urls.py
+++ school_system/tasks/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        
+    path('', views.tasks_view, name='tasks'),
+    path('<int:task_id>/', views.tasks_view, name='task_details'),
+    path('create/', views.create_task_view, name='create_task'),
+    path('edit/<int:task_id>/', views.edit_task_view, name='edit_task'),
+    path('delete/<int:task_id>/', views.delete_task_view, name='delete_task'),
+    
+    path('take/<int:task_id>/', views.take_task_view, name='take_task'),
+    path('drop/<int:task_id>/', views.drop_task_view, name='drop_task'),
+    path('complete/<int:task_id>/', views.complete_task_view, name='complete_task'),
+    path('uncomplete/<int:task_id>/', views.uncomplete_task_view, name='uncomplete_task'),
 ]
