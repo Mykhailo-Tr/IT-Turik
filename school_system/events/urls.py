91e712140edcad222c203a68a282636fa150b75b
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-05-19 20:47:19 +0300

    feat: refactor event views and URLs to use class-based views for improved structure and maintainability

--- school_system/events/urls.py
+++ school_system/events/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from . import views
+from .views import (
+    EventListView, EventCreateView, EventEditView, EventDeleteView,
+    RespondToEventView, LeaveEventView, EventCommentAddView, EventCommentDeleteView
+)
 
 urlpatterns = [
-    path('', views.events_view, name='events'),
-    path('<int:event_id>/', views.events_view, name="event_details"),
-    path('create/', views.create_event_view, name="create_event"),
-    path('edit/<int:event_id>/', views.edit_event_view, name="edit_event"),
-    path('delete/<int:event_id>/', views.delete_event_view, name="delete_event"),
-    path('respond/<int:event_id>/<str:response>/', views.respond_to_event, name='respond_to_event'),
-    path('leave/<int:event_id>/', views.leave_event_view, name='leave_event'),
-    path('<int:event_id>/comment/', views.add_event_comment, name='add_event_comment'),
-    path('comment/<int:comment_id>/delete/', views.delete_event_comment, name='delete_event_comment'),
-
-
+    path('', EventListView.as_view(), name='events'),
+    path('<int:event_id>/', EventListView.as_view(), name="event_details"),
+    path('create/', EventCreateView.as_view(), name="create_event"),
+    path('edit/<int:event_id>/', EventEditView.as_view(), name="edit_event"),
+    path('delete/<int:event_id>/', EventDeleteView.as_view(), name="delete_event"),
+    path('respond/<int:event_id>/<str:response>/', RespondToEventView.as_view(), name='respond_to_event'),
+    path('leave/<int:event_id>/', LeaveEventView.as_view(), name='leave_event'),
+    path('<int:event_id>/comment/', EventCommentAddView.as_view(), name='add_event_comment'),
+    path('comment/<int:comment_id>/delete/', EventCommentDeleteView.as_view(), name='delete_event_comment'),
 ]
--- school_system/events/views.py
+++ school_system/events/views.py
@@ -1,19 +1,78 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.views import View
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
+from django.utils.timezone import now
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
 from django.db.models import Q
-from django.views.decorators.http import require_http_methods, require_GET, require_POST
-from django.shortcuts import get_object_or_404
 from .models import Event, EventParticipation, EventComment
 from .forms import CreateEventForm
 from accounts.models import User
 
-from django.utils.timezone import now
-from django.db.models import Q
 
-@login_required(login_url='login')
-def events_view(request, event_id=None):
-    if event_id:
+class EventListView(LoginRequiredMixin, View):
+    login_url = 'login'
+
+    def get(self, request, event_id=None):
+        if event_id:
+            return self.get_event_detail(request, event_id)
+
+        created_events = Event.objects.filter(author=request.user) if request.user.role != 'admin' else Event.objects.all()
+        invited_events = Event.objects.filter(eventparticipation__user=request.user).exclude(author=request.user)
+
+        q = request.GET.get('q', '').strip()
+        from_date = request.GET.get('from_date')
+        to_date = request.GET.get('to_date')
+        time_filter = request.GET.get('time')
+        status = request.GET.get('status')
+        author = request.GET.get('author')
+
+        if q:
+            invited_events = invited_events.filter(
+                Q(title__icontains=q) | Q(author__first_name__icontains=q) | Q(author__last_name__icontains=q)
+            )
+        if from_date:
+            invited_events = invited_events.filter(start_date__date__gte=from_date)
+        if to_date:
+            invited_events = invited_events.filter(end_date__date__lte=to_date)
+        if time_filter == 'future':
+            invited_events = invited_events.filter(start_date__gte=now())
+        elif time_filter == 'past':
+            invited_events = invited_events.filter(end_date__lt=now())
+        if status:
+            invited_events = invited_events.filter(eventparticipation__user=request.user,
+                                                   eventparticipation__response=status)
+        if author:
+            invited_events = invited_events.filter(author__id=author)
+
+        participations = {
+            p.event_id: p.response for p in EventParticipation.objects.filter(user=request.user)
+        }
+        users = User.objects.all()
+
+        context = {
+            "created_events": created_events,
+            "invited_events": invited_events,
+            "participations": participations,
+            "users": users,
+            "page": "events",
+            "filter": {
+                "q": q,
+                "from_date": from_date,
+                "to_date": to_date,
+                "status": status,
+                "time": time_filter
+            }
+        }
+
+        if request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return render(request, 'events/event_list_partial.html', context)
+
+        return render(request, 'events/events.html', context)
+
+    def get_event_detail(self, request, event_id):
         event = get_object_or_404(Event, id=event_id)
         participation = EventParticipation.objects.filter(event=event, user=request.user).first()
         comments = EventComment.objects.filter(event=event)
@@ -24,147 +83,82 @@ def events_view(request, event_id=None):
             "page": "event_details"
         })
 
-    created_events = Event.objects.filter(author=request.user) if request.user.role != 'admin' else Event.objects.all()
 
-    invited_events = Event.objects.filter(eventparticipation__user=request.user).exclude(author=request.user)
+class EventCommentAddView(LoginRequiredMixin, View):
+    def post(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id)
 
-    q = request.GET.get('q', '').strip()
-    from_date = request.GET.get('from_date')
-    to_date = request.GET.get('to_date')
-    time_filter = request.GET.get('time')
-    status = request.GET.get('status')
-    author = request.GET.get('author')
+        is_author = event.author == request.user
+        participation = EventParticipation.objects.filter(
+            event=event, user=request.user,
+            response=EventParticipation.ResponseChoices.ACCEPTED
+        ).first()
 
-    if q:
-        invited_events = invited_events.filter(
-            Q(title__icontains=q) | Q(author__first_name__icontains=q) | Q(author__last_name__icontains=q)
-        )
+        if not (participation or is_author):
+            messages.error(request, "Only participants who accepted the event or the author can comment.")
+            return redirect('event_details', event_id=event.id)
 
-    if from_date:
-        invited_events = invited_events.filter(start_date__date__gte=from_date)
-    if to_date:
-        invited_events = invited_events.filter(end_date__date__lte=to_date)
+        comment_text = request.POST.get('comment', '').strip()
+        if comment_text:
+            EventComment.objects.create(user=request.user, event=event, comment=comment_text)
 
-    if time_filter == 'future':
-        invited_events = invited_events.filter(start_date__gte=now())
-    elif time_filter == 'past':
-        invited_events = invited_events.filter(end_date__lt=now())
-
-    if status:
-        invited_events = invited_events.filter(eventparticipation__user=request.user,
-                                               eventparticipation__response=status)
-        
-    if author:
-        invited_events = invited_events.filter(author__id=author)
-
-    participations = {
-        p.event_id: p.response for p in EventParticipation.objects.filter(user=request.user)
-    }
-    users = User.objects.all()
-
-
-    context = {
-        "created_events": created_events,
-        "invited_events": invited_events,
-        "participations": participations,
-        "users": users,
-        "page": "events",
-        "filter": {
-            "q": q,
-            "from_date": from_date,
-            "to_date": to_date,
-            "status": status,
-            "time": time_filter
-        }
-    }
-    if request.headers.get("x-requested-with") == "XMLHttpRequest":
-        return render(request, 'events/event_list_partial.html', context)
-
-    return render(request, 'events/events.html', context)
-
-    
-@login_required(login_url='login')
-@require_POST
-def add_event_comment(request, event_id):
-    event = get_object_or_404(Event, id=event_id)
-
-    is_author = event.author == request.user
-    participation = EventParticipation.objects.filter(
-        event=event,
-        user=request.user,
-        response=EventParticipation.ResponseChoices.ACCEPTED
-    ).first()
-
-    if not (participation or is_author):
-        messages.error(request, "Only participants who accepted the event or the author can comment.")
         return redirect('event_details', event_id=event.id)
 
-    comment_text = request.POST.get('comment', '').strip()
-    if comment_text:
-        EventComment.objects.create(user=request.user, event=event, comment=comment_text)
 
-    return redirect('event_details', event_id=event.id)
+class EventCommentDeleteView(LoginRequiredMixin, View):
+    def post(self, request, comment_id):
+        comment = get_object_or_404(EventComment, id=comment_id)
+        event = comment.event
 
+        if comment.user != request.user and event.author != request.user:
+            messages.error(request, "You don't have permission to delete this comment.")
+            return redirect('event_details', event_id=event.id)
 
-
-@login_required
-@require_POST
-def delete_event_comment(request, comment_id):
-    comment = get_object_or_404(EventComment, id=comment_id)
-    event = comment.event
-
-    is_author = event.author == request.user
-    is_comment_owner = comment.user == request.user
-
-    if not (is_comment_owner or is_author):
-        messages.error(request, "You don't have permission to delete this comment.")
+        comment.delete()
+        messages.success(request, "Comment deleted successfully.")
         return redirect('event_details', event_id=event.id)
 
-    comment.delete()
-    messages.success(request, "Comment deleted successfully.")
-    return redirect('event_details', event_id=event.id)
 
-    
-    
-@login_required
-@require_POST
-def respond_to_event(request, event_id, response):
-    event = get_object_or_404(Event, id=event_id)
-    if not EventParticipation.objects.filter(event=event, user=request.user).exists():
-        messages.error(request, "You are not invited to this event.")
-        return redirect('events')
+class RespondToEventView(LoginRequiredMixin, View):
+    def post(self, request, event_id, response):
+        event = get_object_or_404(Event, id=event_id)
 
-    if response not in [choice[0] for choice in EventParticipation.ResponseChoices.choices]:
-        messages.error(request, "Invalid response.")
-        return redirect('events')
+        if not EventParticipation.objects.filter(event=event, user=request.user).exists():
+            messages.error(request, "You are not invited to this event.")
+            return redirect('events')
 
-    participation = EventParticipation.objects.get(event=event, user=request.user)
-    participation.response = response
-    participation.save()
-    messages.success(request, f"You have {response} the event.")
-    return redirect('events')
+        if response not in [choice[0] for choice in EventParticipation.ResponseChoices.choices]:
+            messages.error(request, "Invalid response.")
+            return redirect('events')
 
-
-@login_required
-@require_POST
-def leave_event_view(request, event_id):
-    event = get_object_or_404(Event, id=event_id)
-    participation = get_object_or_404(EventParticipation, user=request.user, event=event)
-
-    if participation.response == EventParticipation.ResponseChoices.ACCEPTED:
-        participation.response = EventParticipation.ResponseChoices.LEAVED
+        participation = EventParticipation.objects.get(event=event, user=request.user)
+        participation.response = response
         participation.save()
-        messages.success(request, "You have left the event.")
-    else:
-        messages.warning(request, "You can only leave an event you have accepted.")
 
-    return redirect('events')
+        messages.success(request, f"You have {response} the event.")
+        return redirect('events')
 
 
-@login_required(login_url='login')
-@require_http_methods(["GET", "POST"])
-def create_event_view(request):
-    if request.method == 'POST':
+class LeaveEventView(LoginRequiredMixin, View):
+    def post(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id)
+        participation = get_object_or_404(EventParticipation, user=request.user, event=event)
+
+        if participation.response == EventParticipation.ResponseChoices.ACCEPTED:
+            participation.response = EventParticipation.ResponseChoices.LEAVED
+            participation.save()
+            messages.success(request, "You have left the event.")
+        else:
+            messages.warning(request, "You can only leave an event you have accepted.")
+        return redirect('events')
+
+
+class EventCreateView(LoginRequiredMixin, View):
+    def get(self, request):
+        form = CreateEventForm(user=request.user)
+        return render(request, 'events/forms/event_form.html', {'form': form})
+
+    def post(self, request):
         form = CreateEventForm(request.POST, user=request.user)
         if form.is_valid():
             event = form.save(commit=False)
@@ -172,29 +166,38 @@ def create_event_view(request):
             event.save()
             form.save_m2m()
 
-            participants = form.cleaned_data['participants']
-            for user in participants:
+            for user in form.cleaned_data['participants']:
                 EventParticipation.objects.create(event=event, user=user)
 
             return redirect('events')
-    else:
-        form = CreateEventForm(user=request.user)
-
-    return render(request, 'events/forms/event_form.html', {'form': form})
+        return render(request, 'events/forms/event_form.html', {'form': form})
 
 
-@login_required(login_url="login")
-@require_http_methods(["GET", "POST"])
-def edit_event_view(request, event_id):
-    event = get_object_or_404(Event, id=event_id)
+class EventEditView(LoginRequiredMixin, UserPassesTestMixin, View):
+    def test_func(self):
+        return self.get_object().author == self.request.user
 
-    if request.user != event.author:
-        messages.error(request, 'You do not have permission to edit this event.')
-        return redirect('events')
+    def get_object(self):
+        return get_object_or_404(Event, id=self.kwargs['event_id'])
 
-    if request.method == 'POST':
+    def get(self, request, event_id):
+        event = self.get_object()
+        form = CreateEventForm(instance=event)
+        selected_participants = User.objects.filter(eventparticipation__event=event)
+
+        form.initial['participants'] = selected_participants
+        form.fields['participants'].queryset = (
+            User.objects.exclude(id=request.user.id) | selected_participants
+        ).distinct()
+
+        return render(request, 'events/forms/event_form.html', {
+            'form': form, 'event': event, 'page': 'edit_event'
+        })
+
+    def post(self, request, event_id):
+        event = self.get_object()
         form = CreateEventForm(request.POST, instance=event)
-        print(form)
+
         if form.is_valid():
             event = form.save(commit=False)
             event.author = request.user
@@ -203,48 +206,28 @@ def edit_event_view(request, event_id):
 
             selected_participants = form.cleaned_data['participants']
             current_participants = User.objects.filter(eventparticipation__event=event)
-            print(selected_participants, current_participants)
 
             for user in current_participants:
                 if user not in selected_participants:
                     EventParticipation.objects.filter(event=event, user=user).delete()
-
             for user in selected_participants:
                 EventParticipation.objects.get_or_create(event=event, user=user)
 
             messages.success(request, 'Event updated successfully.')
             return redirect('events')
-    else:
-        form = CreateEventForm(instance=event)
 
-        selected_participants = User.objects.filter(eventparticipation__event=event)
-
-        form.initial['participants'] = selected_participants
-
-        form.fields['participants'].queryset = (
-            User.objects.exclude(id=request.user.id) | selected_participants
-        ).distinct()
+        return render(request, 'events/forms/event_form.html', {
+            'form': form, 'event': event, 'page': 'edit_event'
+        })
 
 
-    form.fields['participants'].queryset = User.objects.exclude(id=request.user.id)
+class EventDeleteView(LoginRequiredMixin, UserPassesTestMixin, View):
+    def test_func(self):
+        event = get_object_or_404(Event, id=self.kwargs['event_id'])
+        return event.author == self.request.user
 
-    context = {
-        'page': 'edit_event',
-        'form': form,
-        'event': event,
-    }
-    return render(request, 'events/forms/event_form.html', context)
-
-
-
-@login_required(login_url="login")
-@require_http_methods(["GET", "POST"])
-def delete_event_view(request, event_id):
-    if request.user != Event.objects.get(id=event_id).author:
-        messages.error(request, 'You do not have permission to delete this event.')
+    def post(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id)
+        event.delete()
+        messages.success(request, 'Event deleted successfully.')
         return redirect('events')
-    event = Event.objects.get(id=event_id)
-    event.delete()
-    messages.success(request, 'Event deleted successfully.')
-    return redirect('events')
-
